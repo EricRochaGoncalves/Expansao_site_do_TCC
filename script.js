d5e3b3678b0a5c28eeaf46fe65fdaeb6aedf80c0
Eric Gonçalves <166281170+EricRochaGoncalves@users.noreply.github.com>
2024-11-05 22:27:57 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -39,3 +39,16 @@ window.addEventListener('click', (event) => {
   }
 
 })
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute("href");
+        const targetElement = document.querySelector(targetId);
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+    });
+});
+
