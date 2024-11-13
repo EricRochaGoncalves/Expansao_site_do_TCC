39dc1e7963a6d6955a984b95c94f9428440a99fc
Eric Gonçalves <166281170+EricRochaGoncalves@users.noreply.github.com>
2024-11-13 11:19:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -51,4 +51,21 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         }
     });
 });
+let currentIndex = 0;
+const images = document.querySelectorAll('.carrossel img');
+
+function mudarImagem(direction) {
+    currentIndex += direction;
+    if (currentIndex < 0) {
+        currentIndex = images.length - 1;
+    } else if (currentIndex >= images.length) {
+        currentIndex = 0;
+    }
+    document.querySelector('.carrossel').style.transform = `translateX(-${currentIndex * 600}px)`;
+}
+
+// Chama mudarImagem com intervalos para automatizar a troca das imagens
+setInterval(() => mudarImagem(1), 3000); // Muda a imagem a cada 3 segundos
+
+//  Final dos slides
 
